744c73fd9869c0080c3fdc436d98300c6e751843
ottokyosti <90601627+ottokyosti@users.noreply.github.com>
2023-05-17 11:44:41 +0300

    feat: user deletion, delete icons

--- rest-ios-project/rest-ios-project/ContentView.swift
+++ rest-ios-project/rest-ios-project/ContentView.swift
@@ -18,12 +18,12 @@ struct ContentView: View {
             
             TestView()
                 .tabItem {
-                    Label("keskel", systemImage: "cloud.fill")
+                    Label("Search", systemImage: "magnifyingglass")
                 }
             
             TestView()
                 .tabItem {
-                    Label("oikeel", systemImage: "moon.fill")
+                    Label("Add user", systemImage: "person.fill.badge.plus")
                 }
         }
     }
@@ -31,16 +31,28 @@ struct ContentView: View {
 
 struct MainView: View {
     @StateObject var client = HttpClient()
+    @State var showTrashIcons = false
     
     var body: some View {
-        List {
-            if let userData = client.userData {
-                ForEach(userData, id: \.id) { user in
-                    ListView(user: user)
+        NavigationView {
+            MainContent(client: client, showTrashIcons: $showTrashIcons)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack {
+                            Button {
+                                showTrashIcons = !showTrashIcons
+                            } label: {
+                                if (!showTrashIcons) {
+                                    Image(systemName: "trash")
+                                        .foregroundColor(Color.black)
+                                } else {
+                                    Image(systemName: "trash.fill")
+                                        .foregroundColor(Color.black)
+                                }
+                            }
+                        }
+                    }
                 }
-            } else {
-                ProgressView()
-            }
         }
         .onAppear {
             client.getAll(url: "https://dummyjson.com/users")
@@ -48,29 +60,94 @@ struct MainView: View {
     }
 }
 
-struct ListView: View {
-    var user : User
+struct MainContent: View {
+    @ObservedObject var client: HttpClient
+    @Binding var showTrashIcons: Bool
     
     var body: some View {
-        HStack {
-            AsyncImage(url: URL(string: user.image), content: { image in
-                image.resizable()
-            }, placeholder: {
-                ProgressView()
-            })
-            .frame(width: 50, height: 50)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.blue, lineWidth: 2)
-            )
-            VStack (alignment: .leading) {
-                Text("\(user.firstName) \(user.lastName)")
-                    .fontWeight(.bold)
-                Text("Age: \(user.age)")
+        ScrollView {
+            VStack(spacing: 15) {
+                if let userData = client.userData {
+                    ForEach(userData, id: \.id) { user in
+                        ListView(user: user,
+                                 showTrashIcons: $showTrashIcons,
+                                 client: client)
+                    }
+                } else {
+                    ProgressView()
+                }
             }
-            .padding(2)
-            Spacer()
+            .padding()
+        }
+    }
+}
+
+struct ListView: View {
+    var user: User
+    @Binding var showTrashIcons: Bool
+    @ObservedObject var client: HttpClient
+    
+    var body: some View {
+        ZStack {
+            Color.white
+            HStack {
+                AsyncImage(url: URL(string: user.image), content: { image in
+                    image.resizable()
+                }, placeholder: {
+                    ProgressView()
+                })
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(.blue, lineWidth: 2)
+                )
+                VStack(alignment: .leading) {
+                    Text("\(user.firstName) \(user.lastName)")
+                        .fontWeight(.bold)
+                    Text("Age: \(user.age)")
+                }
+                .padding(2)
+                Spacer()
+                MyDeleteButton(user: user,
+                               showTrashIcons: $showTrashIcons,
+                               client: client)
+            }
+            .padding(10)
+        }
+        .cornerRadius(16)
+        .shadow(color: Color.black.opacity(0.5), radius: 8, x: 0, y: 4)
+    }
+}
+
+struct MyDeleteButton: View {
+    var user: User
+    @State var showConfirmDialog = false
+    @Binding var showTrashIcons: Bool
+    @ObservedObject var client: HttpClient
+    
+    var body: some View {
+        Button {
+            showConfirmDialog = !showConfirmDialog
+        } label: {
+            Image(systemName: "trash")
+                .foregroundColor(Color.black)
+                .scaleEffect(showTrashIcons ? 1.25 : 0.001)
+                .animation(.spring(), value: showTrashIcons)
+        }
+        .disabled(!showTrashIcons)
+        .padding(5)
+        .alert(isPresented: $showConfirmDialog) {
+            Alert(
+                title: Text("Deleting user"),
+                message: Text("Are you sure?"),
+                primaryButton: .default(Text("Confirm")) {
+                    client.deleteUser(url: "https://dummyjson.com/users/\(user.id)")
+                },
+                secondaryButton: .default(Text("Cancel")) {
+                    showConfirmDialog = false
+                }
+            )
         }
     }
 }
--- rest-ios-project/rest-ios-project/Models.swift
+++ rest-ios-project/rest-ios-project/Models.swift
@@ -10,11 +10,23 @@ import Alamofire
 
 class HttpClient: ObservableObject {
     @Published var userData: Array<User>? = nil
+    @Published var isDeleteSuccess = false
     
     func getAll(url: String) {
         AF.request(url).responseDecodable(of: Users.self) { response in
             guard let data = response.value else { return }
             self.userData = data.users
+        }
+    }
+    
+    func deleteUser(url: String) {
+        AF.request(url, method: .delete).response { response in
+            switch(response.result) {
+            case .success:
+                self.isDeleteSuccess = true
+            case .failure(let error):
+                print("Error: \(error)")
+            }
         }
     }
 }
